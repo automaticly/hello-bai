afa7734ef2317936530ccfe5bd0aec381feeb73f
baizhaolei <baizhaolei@kuaishou.com>
2022-04-04 00:32:25 +0800

    feat: 编译及请求

---
+++ config-overrides.js
@@ -0,0 +1,18 @@
+const path = require('path');
+module.exports = function override(config, env) {
+    // 参数中的 config 就是默认的 webpack config
+    const loaders = config.module.rules.find(rule => Array.isArray(rule.oneOf)).oneOf;
+    console.log(loaders);
+    loaders[3] = {
+        test: /\.(js|mjs|jsx|ts|tsx)$/,
+        include: path.join(__dirname, 'src'),
+        loader: 'esbuild-loader',
+        options: {
+            loader: 'tsx'
+        }
+    }
+
+
+    // 一定要把新的 config 返回
+    return config;
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+
+  useEffect(()=>{
+    fetch('https://my-typescript-worker.zhaoleibaiupc1399.workers.dev/')
+    .then(responst => responst.json())
+    .then(data => console.log(data));
+  },[])
+
+
   return (
     <div className="App">
       <header className="App-header">
